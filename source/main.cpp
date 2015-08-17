1079fe0bc85459a360ddcaa3f666b03b111e92a5
Chris S <gummybassist@gmail.com>
2015-08-17 22:15:00 +0100

    Really bizarre, memory corruption issue... else if not working!

--- source/main.cpp
+++ source/main.cpp
@@ -19,6 +19,16 @@ public:
 
 	}
 
+	int getX()
+	{
+		return x;
+	}
+
+	int getY()
+	{
+		return y;
+	}
+
 	void update()
 	{
 		y += input.circleYAxis() * 3.0f;
@@ -35,15 +45,138 @@ public:
 
 	void draw()
 	{
-		gfx.renderOnTop([&]{
-			gfx.drawRectangle(20, y, 20, 80, Colour(0xff, 0xff, 0xff, 0xff));
-		});
+		gfx.drawRectangle(x, y, 20, 80, Colour(0xff, 0xff, 0xff, 0xff));
 	}
 
 private:
-	int y { 60 };
+	int x { 20 };
+	int y { 80 };
 	Gfx &gfx;
 	Input &input;
+};
+
+class Ball
+{
+public:
+	Ball(Gfx &graphics) : gfx(graphics)
+	{
+
+	}
+
+	~Ball()
+	{
+
+	}
+
+	void setX(float xVal)
+	{
+		x = xVal;
+	}
+
+	void setY(float yVal)
+	{
+		y = yVal;
+	}
+
+	int getX()
+	{
+		return x;
+	}
+
+	int getY()
+	{
+		return y;
+	}
+
+	void update()
+	{
+		x += xspeed;
+		if (x > 380)
+		{
+			x = 380;
+			flipXSpeed();
+		}
+		else if(x < 0)
+		{
+			x = 0;
+			flipXSpeed();
+		}
+		
+		y += yspeed;
+		if (y > 220)
+		{
+			y = 220;
+			flipYSpeed();
+		}
+		else if(y < 0)
+		{
+			y = 0;
+			flipYSpeed();
+		}
+	}
+
+	void flipYSpeed()
+	{
+		yspeed *= -1.0f;
+	}
+
+	void flipXSpeed()
+	{
+		xspeed *= -1.0f;
+	}
+
+	void draw()
+	{
+		gfx.drawRectangle(x, y, 20, 20, Colour(0xff, 0xff, 0xff, 0xff));
+	}
+
+private:
+	float xspeed { 2.5f };
+	float yspeed { 2.5f };
+	int y { 80 };
+	int x { 80 };
+	Gfx &gfx;
+};
+
+class CollisionHandler
+{
+public:
+	CollisionHandler(PongPaddle &paddle, Ball &targetBall) : pongPaddle(paddle), ball(targetBall) {}
+
+	~CollisionHandler() {}
+
+	void checkCollision()
+	{
+		// Colliding with right side of paddle
+		bool collidingRight = pointInRange(pongPaddle.getX()+20, ball.getX(), ball.getX()+20) && (pointInRange(ball.getY(), pongPaddle.getY(), pongPaddle.getY()+80) || pointInRange(ball.getY()+20, pongPaddle.getY(), pongPaddle.getY()+80));
+		bool collidingUnder = pointInRange(pongPaddle.getY()+80, ball.getY(), ball.getY()+20) && (pointInRange(ball.getX(), pongPaddle.getX(), pongPaddle.getX()+20) || pointInRange(ball.getX()+20, pongPaddle.getX(), pongPaddle.getX()+20));
+		bool collidingTop = pointInRange(pongPaddle.getY(), ball.getY(), ball.getY()+20) && (pointInRange(ball.getX(), pongPaddle.getX(), pongPaddle.getX()+20) || pointInRange(ball.getX()+20, pongPaddle.getX(), pongPaddle.getX()+20));
+		if (collidingRight)
+		{
+			ball.flipXSpeed();
+			ball.setX(pongPaddle.getX()+20);
+		}
+		
+		if (collidingUnder && !collidingRight)
+		{
+			ball.flipYSpeed();
+			ball.setY(pongPaddle.getY()+80);
+		}
+
+		if (collidingTop && !collidingRight)
+		{
+			ball.flipYSpeed();
+			ball.setY(pongPaddle.getY()-20);
+		}
+	}
+
+	bool pointInRange(int point, int rangeStart, int rangeEnd)
+	{
+		return point >= rangeStart && point <= rangeEnd;
+	}
+private:
+	PongPaddle &pongPaddle;
+	Ball &ball;
 };
 
 int main(int argc, char **argv)
@@ -52,6 +185,8 @@ int main(int argc, char **argv)
 	Input input;
 
 	PongPaddle paddle(gfx, input);
+	Ball ball(gfx);
+	CollisionHandler collisionHandler(paddle, ball);
 
 	// Main loop
 	while (aptMainLoop() && !input.startDown())
@@ -59,7 +194,14 @@ int main(int argc, char **argv)
 		input.scan();
 		
 		paddle.update();
-		paddle.draw();
+		ball.update();
+
+		collisionHandler.checkCollision();
+
+		gfx.renderOnTop([&]{
+			ball.draw();
+			paddle.draw();	
+		});
 
 		gfx.flip();
 	}
